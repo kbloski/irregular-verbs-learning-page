9431689a647195838473d2dd2ebb58e19faf270a
Kamil Błoński <kblonski02@gmail.com>
2025-01-31 01:19:27 +0100

    Init new project nextjs

---
+++ src/types/VerbType.ts
@@ -0,0 +1,26 @@
+export enum VerbGroupType {
+    FULLY_IRREGULAR = "FULLY_IRREGULAR",
+    IDENTICALS = "IDENTICALS",
+    FIRST_DIFFERENT = "FIRST_DIFFERENT",
+    MIDDLE_DIFFERENT = "MIDDLE_DIFFERENT",
+    VERB_en = "VERB_en",
+    VERB_an_un = "VERB_an_un",
+    VERB_wn = "VERB_wn",
+}
+
+export type VerbConfigurationType = {
+        id: number,
+        groupType: VerbGroupType,
+        verb: {
+            tense: string,
+            translation: string,
+        },
+        past: {
+            tense: string,
+            translation: string,
+        },
+        pastParticiple: {
+            tense: string,
+            translation: string,
+        },
+}
